ead3ce0e3f0226b5b7d9a988914c274772bc2408
Mauricio Portillo <mauricio.portillo@valkimia.com>
2022-06-17 12:40:14 -0300

    First commit

---
+++ client-side/src/App.js
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">E-Commerce App 2022</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
