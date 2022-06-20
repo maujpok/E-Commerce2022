e03cd1075d8042c093940f940344135591afa486
Mauricio Portillo <mauricio.portillo@valkimia.com>
2022-06-20 19:07:55 -0300

    adding signin route and jwt token

--- server-side/server.js
+++ server-side/server.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import seedRouter from "./routes/seedRoutes.js";
 import productRouter from "./routes/productRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 dotenv.config();
 mongoose
@@ -15,8 +16,17 @@ mongoose
   });
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/seed", seedRouter);
 app.use("/products", productRouter);
+app.use("/users", userRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
